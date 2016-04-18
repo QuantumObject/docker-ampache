abcc059141315ca1cef0da57a5bc6c7319e75858
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-18 17:19:50 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,6 +10,16 @@ set -e
  sed -i 's/post_max_size = 8M/post_max_size = 40M/' /etc/php5/apache2/php.ini
  rm -R /var/www/html/
  
+ #install ampache dependence using composer
+ #install composer first
+ cd /var/www/ampache
+ php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+ php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ php composer-setup.php
+ php -r "unlink('composer-setup.php');"
+ mv composer.phar /usr/local/bin/composer
+ composer install
+ 
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
