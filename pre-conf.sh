e36c12eb8fbae45ed03493049bf382bf95cbc5d9
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-11-14 22:20:07 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -14,7 +14,7 @@ set -e
  #install composer first
  cd /var/www/ampache
  php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
- php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
  php composer-setup.php
  php -r "unlink('composer-setup.php');"
  mv composer.phar /usr/local/bin/composer
