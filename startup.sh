88c4a8eb22c4bc9539f9b069765a189d7084c6bf
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-05-19 17:12:24 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,8 +5,10 @@ set -e
 if [ -f /etc/configured ]; then
         echo 'already configured'
 else
-      #code that need to run only one time ....
-        
+        #code that need to run only one time ....
+        chmod 777 /var/www/ampache/config -R
+        chown -R www-data:www-data /var/www/ampache
+       
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
