a138e2db498e10584d6f8e934f9d311611de0432
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-05-29 16:23:20 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,12 +6,12 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
         #code that need to run only one time ....
-        
-        chmod 777 /var/www/ampache/config -R
-        chown -R www-data:www-data /var/www/ampache
         if [[ ! -f /var/www/config/ampache.cfg.php ]]; then
           mv /var/temp/ampache.cfg.php.dist /var/www/config/ampache.cfg.php.dist
         fi
+        chmod 777 /var/www/ampache/config -R
+        chown -R www-data:www-data /var/www/ampache
+    
        
         #needed for fix problem with ubuntu and cron
         update-locale 
