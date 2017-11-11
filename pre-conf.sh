89ce42d9e337c22083a344c185c2f3926dbd48b6
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-11 13:14:19 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -6,15 +6,15 @@ set -e
  chmod 777 /var/www/ampache/config -R
  chown -R www-data:www-data /var/www/ampache
  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/ampache/' /etc/apache2/sites-enabled/000*.conf
- sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/' /etc/php5/apache2/php.ini
- sed -i 's/post_max_size = 8M/post_max_size = 40M/' /etc/php5/apache2/php.ini
+ sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/' /etc/php/7.0/apache2/php.ini 
+ sed -i 's/post_max_size = 8M/post_max_size = 40M/' /etc/php/7.0/apache2/php.ini 
  rm -R /var/www/html/
  
  #install ampache dependence using composer
  #install composer first
  cd /var/www/ampache
  php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
- php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
  php composer-setup.php
  php -r "unlink('composer-setup.php');"
  mv composer.phar /usr/local/bin/composer
