caafcc07ea634d73c25bba202364d7504fe45e79
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-11 15:17:49 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -13,13 +13,25 @@ set -e
  #install ampache dependence using composer
  #install composer first
  cd /var/www/ampache
- php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
- php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
- php composer-setup.php
- php -r "unlink('composer-setup.php');"
- mv composer.phar /usr/local/bin/composer
- composer install
+ EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+ if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+  then
+     >&2 echo 'ERROR: Invalid installer checksum'
+     rm composer-setup.php
+     exit 1
+  fi
+  php composer-setup.php --quiet
+  RESULT=$?
+  php -r "unlink('composer-setup.php');"
+  mv composer.phar /usr/local/bin/composer
+  composer install
+  rm composer-setup.php
  
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
+
+ exit $RESULT
