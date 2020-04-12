8702314f049ffcdacad075d2a1ef2fc7e828ad69
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-11 22:58:56 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -4,7 +4,6 @@ set -e
  #apache2 conf
  a2enmod rewrite
  chmod 770 /var/www/ampache/config -R
- chown -R www-data:www-data /var/www/ampache
  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/ampache/' /etc/apache2/sites-enabled/000*.conf
  sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/' /etc/php/7.2/apache2/php.ini 
  sed -i 's/post_max_size = 8M/post_max_size = 40M/' /etc/php/7.2/apache2/php.ini 
@@ -15,7 +14,7 @@ cd /var/www/ampache
 cp ./rest/.htaccess.dist ./rest/.htaccess
 cp ./play/.htaccess.dist ./play/.htaccess
 cp ./channel/.htaccess.dist ./channel/.htaccesss
- 
+chown -R www-data:www-data /var/www/ampache
  
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
