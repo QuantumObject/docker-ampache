634e1915c5336ab6cd5d32e2fe03d79915b2f3d3
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-11 22:03:16 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,6 +10,17 @@ set -e
  sed -i 's/post_max_size = 8M/post_max_size = 40M/' /etc/php/7.2/apache2/php.ini 
  rm -R /var/www/html/
  
+ #copy the .htaccess 
+cd /var/www/ampache
+cp ./rest/.htaccess.dist ./rest/.htaccess
+cp ./play/.htaccess.dist ./play/.htaccess
+cp ./channel/.htaccess.dist ./channel/.htaccesss
+ 
+ 
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
+ 
+ # create a backup for when using docker volume
+ cp -Rp /var/www/ampache/config /var/backups
+ cp -Rp /var/www/ampache/themes /var/backups
